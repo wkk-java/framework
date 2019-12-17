301f350d527d7b2d4f5d04efa2da35d667b55bb1
vince <103420080@qq.com>
2019-12-17 23:43:14 +0800

    eureka加入用户密码

---
+++ eureka-server/src/main/java/com/wk/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.wk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * spring cloud2.0需加入这个配置,否则eureka client注册不成功.
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //关闭csrf
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+    }
+
+}
