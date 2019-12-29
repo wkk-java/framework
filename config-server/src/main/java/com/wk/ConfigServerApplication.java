888a47709512667421764a6c53a542bf3d4dc92f
vince <103420080@qq.com>
2019-12-30 00:21:47 +0800

    todo: "服务器地址"配置抽离git branch ->windows/mac

--- config-server/src/main/java/com/wk/ConfigServerApplication.java
+++ config-server/src/main/java/com/wk/ConfigServerApplication.java
@@ -6,13 +6,21 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author wkk
+ * 启动类.
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
 @RefreshScope
 public class ConfigServerApplication {
 
+    /**
+     * 主方法.
+     * @param args 参数
+     */
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
