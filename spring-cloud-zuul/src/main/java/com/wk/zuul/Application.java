5c6c1f1edf42e78247666293bb8419266ef5bb88
vince <wukunkun@bakejinfu.com>
2020-07-21 00:06:35 +0800

    stash

--- spring-cloud-zuul/src/main/java/com/wk/zuul/Application.java
+++ spring-cloud-zuul/src/main/java/com/wk/zuul/Application.java
@@ -2,11 +2,20 @@ package com.wk.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class Application {
+
+    //配置动态刷新
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
