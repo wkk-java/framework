5ca180208e285a670f90981122f0cb8a56084fbc
wk <103420080@qq.com>
2021-05-13 18:10:15 +0800

    暂存

--- gateway/src/main/java/com/wk/gateway/Application.java
+++ gateway/src/main/java/com/wk/gateway/Application.java
@@ -1,8 +1,12 @@
 package com.wk.gateway;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author: vince
@@ -10,10 +14,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description: 启动类
  */
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("启动成功!");
+    }
+
+    @Bean
+    public IRule loadBalanceRule() {
+        return new NacosRule();
     }
 }
